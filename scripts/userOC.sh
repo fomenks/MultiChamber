dee2513775050d19db940f45a8e6ba9935ecdc8e
Sergey A. Fomenko <fomenks@yandex.ru>
2026-02-23 10:26:28 +0300

    fix: ensure workspace directory exists and source environment
    Create workspace directory if it does not exist
    Source environment variables from .env file when present

--- scripts/userOC.sh
+++ scripts/userOC.sh
@@ -3,6 +3,14 @@
 cd ~
 . ~/.bashrc
 
+if [ -f .env ] ; then
+ source .env
+fi
+
+if [ ! -d workspace ] ; then 
+  mkdir workspace
+fi
+
 export PATH="$PATH:~/.local/bin:/usr/local/go/bin"
 export OC_SSH='user123.'
 export SSHPASS='user123.'
